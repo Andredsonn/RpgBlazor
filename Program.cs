b9f5ed228b3a861eb26a0d37d515d6f54b0d74c6
Andredsonn <andreaesilva6@gmail.com>
2025-11-13 14:45:34 -0300

    compelto

---
+++ Program.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Net.Http;
+using Blazored.LocalStorage;
+using RpgBlazor.Components;
+using RpgBlazor.Services;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// configurar HttpClient com a base da API
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri("https://rpgapids20252.azurewebsites.net/")
+});
+
+builder.Services.AddBlazoredLocalStorage();
+
+//inicialização da classe para uso em outras classes
+builder.Services.AddScoped<UsuarioService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
